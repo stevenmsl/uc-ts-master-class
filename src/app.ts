75730d43cb80c3cc95f599ab3c6c942f21672532
Steven Lin <slin@merchantgrowth.com>
2019-12-04 14:24:35 -0800

    record mapped type

--- src/app.ts
+++ src/app.ts
@@ -159,3 +159,22 @@ const person: MyPick<Person, "name" | "age"> = {
   age: 27
 };
 */
+
+/* record mapped type */
+/*
+let dictionary: Record<string, TrackStates> = {};
+
+interface TrackStates {
+  current: string;
+  next: string;
+}
+
+const item: Record<keyof TrackStates, string> = {
+  current: "js02js9",
+  next: "8nlksjsk"
+};
+
+// Numbers are coerced to String
+dictionary[0] = item;
+console.log(dictionary[0]);
+*/
