7f4a260798e1f91c12f4b0804a25d6f1cecb0689
Steven Lin <slin@merchantgrowth.com>
2019-12-05 14:01:38 -0800

    user defined type guard

--- src/app.ts
+++ src/app.ts
@@ -207,6 +207,7 @@ console.log(songDurationFromMS);
 */
 
 /* instanceof type guard */
+/*
 class Foo {
   bar() {}
 }
@@ -236,3 +237,33 @@ const playlistName = getItemName(
   new Playlist("The Best Songs", [new Song("The Man", 300000)])
 );
 console.log("Playlist name:", playlistName);
+*/
+
+/* user defined type guard */
+/*
+class Song {
+  constructor(public title: string, public duration: number) {}
+}
+class Playlist {
+  constructor(public name: string, public songs: Song[]) {}
+}
+
+function isSong(item: any): item is Song {
+  return item instanceof Song;
+}
+
+function getItemName(item: Song | Playlist) {
+  if (isSong(item)) {
+    return item.title;
+  }
+  return item.name;
+}
+
+const songName = getItemName(new Song("Wonderful Wonderful", 300000));
+console.log("Song name:", songName);
+
+const playlistName = getItemName(
+  new Playlist("The Best Songs", [new Song("The Man", 300000)])
+);
+console.log("Playlist name:", playlistName);
+*/
