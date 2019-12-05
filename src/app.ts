4815effe3e0ba6674563445f7fe9de5559bcf278
Steven Lin <slin@merchantgrowth.com>
2019-12-05 15:54:20 -0800

    intersection types

--- src/app.ts
+++ src/app.ts
@@ -267,3 +267,47 @@ const playlistName = getItemName(
 );
 console.log("Playlist name:", playlistName);
 */
+
+/* in operator literal type guard */
+//Code is not working
+
+/* intersection types */
+/*
+interface Order {
+  id: string;
+  amount: number;
+  currency: string;
+}
+
+interface Stripe {
+  card: string;
+  cvc: string;
+}
+
+interface PayPal {
+  email: string;
+}
+
+type CheckoutCard = Order & Stripe;
+type CheckoutPayPal = Order & PayPal;
+type CheckoutABC = Order & { name: string };
+const order: Order = {
+  id: "xj28s",
+  amount: 100,
+  currency: "USD"
+};
+
+const orderCard: CheckoutCard = {
+  ...order,
+  card: "1000 2000 3000 4000",
+  cvc: "123"
+};
+
+const orderPayPal: CheckoutPayPal = {
+  ...order,
+  email: "abc@def.com"
+};
+
+const assigned = Object.assign({}, order, orderCard);
+console.log(assigned);
+*/
