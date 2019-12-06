3a3de4a0958d5bba2dafbc8bdef3aae82f6f7de6
Steven Lin <slin@merchantgrowth.com>
2019-12-06 14:37:51 -0800

    enum string inlining

--- src/app.ts
+++ src/app.ts
@@ -504,3 +504,20 @@ const selectedSize = 2;
 
 console.log(Sizes.Large, Sizes[selectedSize]); // 2 "Large"
 */
+
+/* enum string inlining */
+/*
+const enum Sizes {
+  Small = "small",
+  Medium = "medium",
+  Large = "large"
+}
+let selected: Sizes = Sizes.Small;
+
+function updateSize(size: Sizes): void {
+  selected = size;
+}
+
+updateSize(Sizes.Large);
+console.log(selected);
+*/
