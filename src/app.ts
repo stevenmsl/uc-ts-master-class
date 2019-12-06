ef572dda9e82d37acc2e48fcd91260f7b4ada3e2
Steven Lin <slin@merchantgrowth.com>
2019-12-06 10:01:22 -0800

    discriminated union types

--- src/app.ts
+++ src/app.ts
@@ -269,6 +269,7 @@ console.log("Playlist name:", playlistName);
 */
 
 /* in operator literal type guard */
+/*
 class Song {
   kind: "song" = "song";
   constructor(public title: string, public duration: number) {}
@@ -339,3 +340,55 @@ const orderPayPal: CheckoutPayPal = {
 const assigned = Object.assign({}, order, orderCard);
 console.log(assigned);
 */
+
+/* discriminated union types */
+
+interface Order {
+  id: string;
+  amount: number;
+  currency: string;
+}
+interface Stripe {
+  type: "stripe"; //literal type
+  card: string;
+  cvc: string;
+}
+interface PayPal {
+  type: "paypal";
+  email: string;
+}
+type CheckoutCard = Order & Stripe;
+type CheckoutPayPal = Order & PayPal;
+
+const order: Order = {
+  id: "xj28s",
+  amount: 100,
+  currency: "USD"
+};
+
+const orderCard: CheckoutCard = {
+  ...order,
+  type: "stripe",
+  card: "1000 2000 3000 4000",
+  cvc: "123"
+};
+
+const orderPayPal: CheckoutPayPal = {
+  ...order,
+  type: "paypal",
+  email: "abc@def.com"
+};
+
+type Payload = CheckoutCard | CheckoutPayPal;
+
+function checkout(payload: Payload) {
+  if (payload.type === "stripe") {
+    console.log(payload.card, payload.cvc);
+  }
+  if (payload.type === "paypal") {
+    console.log(payload.email);
+  }
+}
+
+checkout(orderCard);
+checkout(orderPayPal);
