e14f464f6f3333f4d7eda60a8ab63dc49dcdc649
Steven Lin <slin@merchantgrowth.com>
2019-12-05 16:21:06 -0800

    in operator literal type guard

--- src/app.ts
+++ src/app.ts
@@ -269,7 +269,35 @@ console.log("Playlist name:", playlistName);
 */
 
 /* in operator literal type guard */
-//Code is not working
+class Song {
+  kind: "song" = "song";
+  constructor(public title: string, public duration: number) {}
+}
+
+class Playlist {
+  kind: "playlist" = "playlist";
+  constructor(public name: string, public songs: Song[]) {}
+}
+
+function isSong(item: any): item is Song {
+  return "title" in item;
+}
+
+function getItemName(item: Song | Playlist) {
+  // if (isSong(item)) {
+  if (item.kind === "song") {
+    return item.title;
+  }
+  return item.name;
+}
+
+const songName = getItemName(new Song("Wonderful Wonderful", 300000));
+console.log("Song name:", songName);
+
+const playlistName = getItemName(
+  new Playlist("The Best Songs", [new Song("The Man", 300000)])
+);
+console.log("Playlist name:", playlistName);
 
 /* intersection types */
 /*
